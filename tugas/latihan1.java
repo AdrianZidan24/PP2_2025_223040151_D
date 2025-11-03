2d20188bdf78aa910b7a34868af25b85789da0a1
zidan <adrian.223040151@mail.unpas.ac.id>
2025-11-03 15:19:39 +0700

    Tugas2 Latihan1

---
+++ tugas/latihan1.java
@@ -0,0 +1,52 @@
+package tugas;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+
+public class latihan1 {
+    public static void main(String[] args) {
+        //1. buat frame 
+        JFrame frame = new JFrame("Contoh BorderLayout");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,400);
+
+        //2. buat layar di bagian atas menggunakan JTextField
+        JTextField layar = new JTextField();
+        frame.add(layar, BorderLayout.NORTH);
+
+        //3. Buat panel untuk tombol dengan GridLayout 4 baris, 4 kolom
+        JPanel panelTombol = new JPanel();
+        panelTombol.setLayout(new GridLayout(4, 4, 5, 5));
+
+        //4. Tambahkan 16 tombol (0-9 dan operator +,-,*./)
+        panelTombol.add(new JButton("7"));
+        panelTombol.add(new JButton("8"));
+        panelTombol.add(new JButton("9"));
+        panelTombol.add(new JButton("/"));
+        panelTombol.add(new JButton("4"));
+        panelTombol.add(new JButton("5"));
+        panelTombol.add(new JButton("6"));
+        panelTombol.add(new JButton("*"));
+        panelTombol.add(new JButton("1"));
+        panelTombol.add(new JButton("2"));
+        panelTombol.add(new JButton("3"));
+        panelTombol.add(new JButton("-"));
+        panelTombol.add(new JButton("0"));
+        panelTombol.add(new JButton("."));
+        panelTombol.add(new JButton("="));
+        panelTombol.add(new JButton("+"));
+
+    //5. Tambahkan panel ke frame di bagian CENTER
+    // frame.add(panelTombol, BorderLayout.CENTER);    
+    frame.add(panelTombol, BorderLayout.CENTER);
+
+    //6. Tampilkan Frame
+    frame.setVisible(true);
+    }
+    
+}
+
